50acafc827a6221b9abbef0c87a25f94b970f92c
Vladislav Petković <121941612+SaladVlad@users.noreply.github.com>
2024-12-18 14:04:14 +0100

    Windows Auth & Certificate Fetch for Credential Store

--- CredentialsStore/Program.cs
+++ CredentialsStore/Program.cs
@@ -6,6 +6,9 @@ using System.Security.Principal;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel.Security;
 
 namespace CredentialsStore
 {
@@ -18,16 +21,42 @@ namespace CredentialsStore
             string addressClient = "net.tcp://localhost:5000/CredentialsStore";
             string addressAuthentificationService = "net.tcp://localhost:6000/CredentialsStore";
 
+            // Windows auth protocol init
+
+            bindingClient.Security.Mode = SecurityMode.Transport;
+            bindingClient.Security.Transport.ClientCredentialType = TcpClientCredentialType.Windows;
+            bindingClient.Security.Transport.ProtectionLevel = ProtectionLevel.EncryptAndSign;
+
             ServiceHost hostCredentialsStore = new ServiceHost(typeof(CredentialsStore));
             ServiceHost hostAuthenticationServiceManagement = new ServiceHost(typeof(AuthenticationServiceManagement));
+
+
+            // Certificate config
+
+            string serverName = CertificateFormatter.ParseName(WindowsIdentity.GetCurrent().Name);
+            bindingAuthentificationService.Security.Transport.ClientCredentialType = TcpClientCredentialType.Certificate;
+            hostAuthenticationServiceManagement.Credentials.ClientCertificate.Authentication.CertificateValidationMode = X509CertificateValidationMode.ChainTrust;
+            hostAuthenticationServiceManagement.Credentials.ClientCertificate.Authentication.RevocationMode = X509RevocationMode.NoCheck;
+            hostAuthenticationServiceManagement.Credentials.ServiceCertificate.Certificate = CertificateManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, "credentialsstore");
 
             hostCredentialsStore.AddServiceEndpoint(typeof(IAccountManagement), bindingClient, addressClient);
             hostAuthenticationServiceManagement.AddServiceEndpoint(typeof(IAuthenticationServiceManagement), bindingAuthentificationService, addressAuthentificationService);
 
             hostCredentialsStore.Open();
+
+            try
+            {
+                hostAuthenticationServiceManagement.Open();
+                
+            }
+            catch (InvalidOperationException) // if the certificate configuration is not successful
+            {
+                Console.WriteLine("Certificate check failed. Please provide the correct certificates.\n");
+                hostCredentialsStore.Abort(); //close the other open host in case of error
+            }
             hostAuthenticationServiceManagement.Open();
 
-            Console.WriteLine($"Credentials store servis successfully started by [{WindowsIdentity.GetCurrent().User}] -> " + WindowsIdentity.GetCurrent().Name + ".\n");
+            Console.WriteLine($"Credentials store service successfully started by [{WindowsIdentity.GetCurrent().User}] -> " + WindowsIdentity.GetCurrent().Name + ".\n");
 
             Console.ReadLine();
 
