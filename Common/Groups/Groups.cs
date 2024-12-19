bf95739fb41a6dd063f6a59456cde56199ae5791
tomicdusann <148521690+tomicdusann@users.noreply.github.com>
2024-12-19 15:41:35 +0100

    Groups class for fetching user based on OS user group

---
+++ Common/Groups/Groups.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Groups
+{
+    public class Groups
+    {
+        private static string generalUser = "AccountUsers";
+        private static string adminUser = "AccountAdmins";
+
+        public static string GeneralUser { get => generalUser; set => generalUser = value; }
+        public static string AdminUser { get => adminUser; set => adminUser = value; }
+    }
+}
