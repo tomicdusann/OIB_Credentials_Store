8e439108d914e2be6929baf75832308151fb50a8
LukaSaric9 <lukisaric@gmail.com>
2025-01-19 14:44:12 +0100

    Adding methods in C.S. Proxy and generating secret key in service program

--- Team9_OIB/Program.cs
+++ Team9_OIB/Program.cs
@@ -1,4 +1,6 @@
-﻿using Common;
+﻿using AuthenticationService.Data;
+using Common;
+using Common.Cryptography;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +16,10 @@ namespace Team9_OIB
     {
         static void Main(string[] args)
         {
+
+            string secretKey = SecretKey.GenerateKey();
+            SecretKey.StoreKey(secretKey, AES.KeyLocation);
+
             NetTcpBinding bindingClient = new NetTcpBinding();
             string address = "net.tcp://localhost:4000/AuthenticationService";
 
@@ -34,6 +40,10 @@ namespace Team9_OIB
 
 
             Console.ReadKey();
+
+            CurrentUsers users = new CurrentUsers();
+            users.resetCurrentUsers(); 
+
             host.Close();
         }
     }
