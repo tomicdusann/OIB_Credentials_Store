2053ac503bb1a4404fd83a216462536b48ec3f71
tomicdusann <148521690+tomicdusann@users.noreply.github.com>
2025-03-18 20:25:56 +0100

    Sign In and Lock acc options fully implemented

--- Client/Program.cs
+++ Client/Program.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net.Security;
 using Common.Exceptions;
+using System.Threading;
 
 namespace Client
 {
@@ -31,7 +32,7 @@ namespace Client
             string username = "";
             string password = "";
 
-            using(AuthenticationProxy authentication = new AuthenticationProxy(binding, authenticationServiceAddress))
+            using(AuthenticationProxy authenticationProxy = new AuthenticationProxy(binding, authenticationServiceAddress))
             {
                 using (CredentialsStoreProxy credentialsStoreProxy = new CredentialsStoreProxy(binding, credentialsStoreAddress))
                 {
@@ -49,6 +50,69 @@ namespace Client
 
                         switch (menu)
                         {
+                            case "1":
+                                if (username == "")
+                                {
+                                    Console.Write("Your username: ");
+                                    username = Console.ReadLine();
+                                    Console.Write("Your password: ");
+                                    password = Console.ReadLine();
+
+                                    try
+                                    {
+                                        int ret = authenticationProxy.Login(username, password);
+
+                                        switch (ret)
+                                        {
+                                            case -4:
+                                                Console.WriteLine("Your data may be TAMPERED with. Please contact your system administrator.\n");
+                                                username = "";
+                                                break;
+                                            case -3:
+                                                Console.WriteLine($"\n{username} is DISABLED. Please contact your system administrator.\n");
+                                                username = "";
+                                                break;
+                                            case -2:
+                                                Console.WriteLine($"\n{username} is LOCKED. Please contact your system administrator or wait some time and try again.\n");
+                                                username = "";
+                                                break;
+                                            case -1:
+                                                Console.WriteLine($"\n{username} does not exist in our Database. Please contact your system administrator.\n");
+                                                username = "";
+                                                break;
+                                            case 0:
+                                                Console.WriteLine($"\n{username} password does not match please try again.\n");
+                                                username = "";
+                                                break;
+                                            case 1:
+                                                Console.WriteLine($"\n{username} successfully logged in.\n");
+                                                Task t = Task.Factory.StartNew(() => DoCheck(authenticationProxy, ref username));
+                                                break;
+                                            case 2:
+                                                Console.WriteLine($"\n{username} already logged in, try logging out first.\n");
+                                                username = "";
+                                                break;
+                                        }
+                                    }
+
+                                    catch (FaultException<InvalidGroupException> ex)
+                                    {
+                                        Console.WriteLine(ex.Detail.exceptionMessage);
+                                        username = "";
+                                        break;
+                                    }
+                                    catch (Exception e)
+                                    {
+                                        Console.WriteLine(e.Message);
+                                        username = "";
+                                        break;
+                                    }
+                                }
+                                else
+                                    Console.WriteLine("You are already logged in, please log out first.\n");
+
+                                break;
+
                             case "2":
                                 try
                                 {
@@ -145,9 +209,67 @@ namespace Client
                                     username = "";
                                     break;
                                 }
+
+                            case "6":
+                                try
+                                {
+                                    Console.Write("Account username: ");
+                                    username = Console.ReadLine();
+                                    credentialsStoreProxy.LockAccount(username);
+                                    Console.WriteLine("Account successfully locked.\n");
+                                    username = "";
+                                    break;
+                                }
+                                catch (FaultException<InvalidGroupException> ex)
+                                {
+                                    Console.WriteLine(ex.Detail.exceptionMessage);
+                                    username = "";
+                                    break;
+                                }
+                                catch (FaultException<InvalidUserException> ex)
+                                {
+                                    Console.WriteLine(ex.Detail.exceptionMessage);
+                                    username = "";
+                                    break;
+                                }
                         }
 
                     } while (menu != "quit");
+                }
+            }
+        }
+
+        static void DoCheck(AuthenticationProxy proxy, ref string user)
+        {
+            while (proxy.State == CommunicationState.Opened)
+            {
+                int result = proxy.CheckIn(user);
+
+                switch (result)
+                {
+                    case -4:
+                        Console.WriteLine("\n{0} Your Account has been TIMED OUT for inactivity.\n", user);
+                        Console.Write("->");
+                        user = "";
+                        return;
+                    case -3:
+                        Console.WriteLine("\n{0} Your Account has been DISABLED and logged out.\n", user);
+                        Console.Write("->");
+                        user = "";
+                        return;
+                    case -2:
+                        Console.WriteLine("\n{0} Your Account has been LOCKED and logged out.\n", user);
+                        Console.Write("->");
+                        user = "";
+                        return;
+                    case -1:
+                        Console.WriteLine("\n{0} Could not be checked, is MISSING from DB and logged out.\n", user);
+                        Console.Write("->");
+                        user = "";
+                        return;
+                    default:
+                        Thread.Sleep(2500);
+                        break;
                 }
             }
         }
